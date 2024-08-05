757616072c635f3b519f7aab1714243150334ee3
Esteban Salvay Dilena <estebansalvay@imajine.com>
2024-08-05 17:06:05 -0300

    feat(mongoDB-conection)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api Documentation')
+    .setDescription('The base nestjs API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
